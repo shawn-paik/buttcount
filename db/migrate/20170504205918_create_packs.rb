13de2c43471a9b554ae9884675e9536bd6885ddf
Shawn Paik <paik.shawn@gmail.com>
2017-05-05 13:10:23 -0400

    delete default

--- db/migrate/20170504205918_create_packs.rb
+++ db/migrate/20170504205918_create_packs.rb
@@ -3,7 +3,7 @@ class CreatePacks < ActiveRecord::Migration[5.0]
     create_table :packs do |t|
     	t.float			:price
     	t.references	:user, null: false
-    	t.integer		:total, default => 20
+    	t.integer		:total, default: 20
     	t.boolean		:finished
     	t.string		:brand
       t.timestamps
